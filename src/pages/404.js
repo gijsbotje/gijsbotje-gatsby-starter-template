e1506e6d58bd6c5894cc9b44ad704c8ad168cc58
gijsbotje <gijs@afosto.io>
2021-03-11 23:48:52 +0100

    fix: fix text

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,33 +1,30 @@
 import React from 'react';
-import { Link } from 'gatsby';
+import PropTypes from 'prop-types';
 import { Helmet } from 'react-helmet';
 import Layout from '../components/Layout';
 import Grid from '../components/Grid';
 import Typography from '../components/Typography';
 
-const NotFound = () => (
-  <Layout>
-    <Helmet title="404 pagina niet gevonden | Afosto" />
+const NotFound = ({ location }) => (
+  <Layout location={location}>
+    <Helmet title="404 page not found" />
     <Grid container className="pt-120 pb-144">
       <Typography variant="h1" className="mb-12">
-        Pagina niet gevonden
+        Page not found
       </Typography>
       <Typography variant="subtitle-1" className="mb-40">
-        Deze pagina is verplaatst of heeft nooit bestaan. Gebruik het menu om verder te gaan.
-      </Typography>
-      <Typography variant="h5">Hulp nodig?</Typography>
-      <Typography variant="body-large" color="gray-700" className="mb-40">
-        Vul ons{' '}
-        <Link to="/contact/" className="hover-underline">
-          contactformulier
-        </Link>{' '}
-        in en wij nemen zo snel mogelijk contact op.
-      </Typography>
-      <Typography variant="body-large" component={Link} to="/" className="hover-underline">
-        Afosto homepage
+        This page was moved or could not be found
       </Typography>
     </Grid>
   </Layout>
 );
+
+NotFound.propTypes = {
+  location: PropTypes.object,
+};
+
+NotFound.defaultProps = {
+  location: {},
+};
 
 export default NotFound;
